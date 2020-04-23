21722eb1fb4b6e37086e1689bef7fc859353138f
github username <github email address>
2020-04-23 07:30:15 +0100

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,27 @@
+#Define a class Person
+class Person
+
+    #Define the setter method for Person name
+    #Into an instance variable @name
+    def name=(person_name)
+        @name = person_name
+    end
+    
+    #Read the name of a Person instance via instance variable @name
+    def name
+        @name
+    end
+    
+    
+    #Define the setter method for Person job
+    #Into an instance variable @job
+    def job=(person_job)
+        @job = person_job
+    end
+    
+    #Read the job of a Person instance via instance variable @job
+    def job
+        @job
+    end
+    
+    end
